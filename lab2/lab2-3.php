2d781d06bc4237efcf501c2c81133a920cf78f8e
Antuane_Specione <boosla@gmail.com>
2023-12-13 23:38:35 +0300

    feat:add images

--- lab2/lab2-3.php
+++ lab2/lab2-3.php
@@ -15,7 +15,7 @@ $b = cos(Z_VALUE) ** 2 + log10(2 * X_VALUE) + abs(Y_VALUE);
 echo "<html><head><title>Результат лабораторной работы</title></head><body>";
 echo "<h1>Лабораторная работа по математическим выражениям</h1>";
 echo "<p>Этот скрипт вычисляет значение выражения b = cos<sup>2</sup>z + lg(2x) + |y|</p>";
-echo "<img src='путь к изображению/Скриншот_1.png' alt='Математическое выражение'>";
+echo "<img src='./img/scr.png' alt='Математическое выражение'>";
 echo "<p>Значение b равно: " . $b . "</p>";
 echo "</body></html>";
 ?>
